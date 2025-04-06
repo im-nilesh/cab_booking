d2a7d8caab107ca8d5fed1cf6c72e1a54a510e27
Nilesh <nileshdadab@gmail.com>
2025-04-06 11:33:43 +0530

    Refactor DriverProfileCreatedScreen: update layout structure, enhance text styling with Google Fonts, and improve image handling for profile display.

--- cab_booking_user/lib/screens/auth/driver/driver_profile_created_screen.dart
+++ cab_booking_user/lib/screens/auth/driver/driver_profile_created_screen.dart
@@ -5,6 +5,7 @@ import 'package:cab_booking_user/Widgets/progress_bar/custom_progress_bar.dart';
 import 'package:cab_booking_user/Widgets/info_box/info_dialog.dart';
 import 'package:cab_booking_user/utils/constants.dart';
 import 'package:cab_booking_user/providers/driver_registration_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DriverProfileCreatedScreen extends ConsumerStatefulWidget {
   const DriverProfileCreatedScreen({Key? key}) : super(key: key);
@@ -27,59 +28,96 @@ class _DriverProfileCreatedScreenState
     final provider = ref.watch(driverRegistrationProvider);
 
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40),
-              const CustomProgressBar(
-                currentStep: 1,
-                totalSteps: 2,
-                label: 'Basic Information',
-              ),
-              const SizedBox(height: 40),
-              CircleAvatar(
-                radius: 130,
-                backgroundColor: avatarborderColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: blackColor),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CustomProgressBar(
+              currentStep: 1,
+              totalSteps: 2,
+              label: 'Basic Information',
+            ),
+            const SizedBox(height: 150),
+            CircleAvatar(
+              radius: 130,
+              backgroundColor: avatarborderColor,
+              child: CircleAvatar(
+                radius: 123,
+                backgroundColor: Colors.white,
                 child: CircleAvatar(
-                  radius: 123,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 115,
-                    backgroundImage:
-                        provider.imageUrl != null
-                            ? NetworkImage(provider.imageUrl!)
-                            : const AssetImage('assets/images/placeholder.png')
-                                as ImageProvider,
+                  radius: 115,
+                  backgroundImage:
+                      provider.imageUrl != null
+                          ? NetworkImage(provider.imageUrl!)
+                          : const AssetImage('assets/images/placeholder.png')
+                              as ImageProvider,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Your profile is created!',
+              style: GoogleFonts.outfit(
+                fontSize: 16,
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: dialogBackgroundColor, // Background color
+                borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                // Border styling
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'To host a ride, you must register your vehicle on this platform.',
+                    style: GoogleFonts.outfit(
+                      fontSize: 12,
+                      color: dialogTextColor,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                ),
+                  SizedBox(height: 8),
+                  Center(
+                    child: Text(
+                      'All it takes is 3 easy steps!',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: dialogTextColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              const Text(
-                'Your profile is created!',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const InfoDialog(
-                text:
-                    'To host a ride, you must register your vehicle on this platform.\nAll it takes is 3 easy steps!',
-              ),
-              const Spacer(),
-              PrimaryButton(
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.07,
+              child: PrimaryButton(
                 text: 'Start',
                 onPressed: () {
                   // Navigate to the next screen
                 },
               ),
-              const SizedBox(height: 20),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
