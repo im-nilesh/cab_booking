5e90ca0b7f557d7496ec2d64b24ffa3b1e77e1a7
Nilesh <nileshdadab@gmail.com>
2025-04-06 12:10:21 +0530

    Add DriverVehicleRegistrationScreen: implement vehicle registration UI with input handling and navigation to required documents screen.

--- cab_booking_user/lib/screens/auth/driver/driver_profile_created_screen.dart
+++ cab_booking_user/lib/screens/auth/driver/driver_profile_created_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cab_booking_user/screens/auth/driver/driver_vehical_registration_screen.dart';
+import 'package:cab_booking_user/screens/auth/driver/driver_vehicle_registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cab_booking_user/Widgets/button/primary_button.dart';
