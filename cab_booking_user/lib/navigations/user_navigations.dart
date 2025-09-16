36ed399694b415d374b76476a0b54efe7f176336
Nilesh <nileshdadab@gmail.com>
2025-09-16 13:34:07 +0530

    Refactor navigation bar and update SVG assets for improved UI consistency

--- cab_booking_user/lib/navigations/user_navigations.dart
+++ cab_booking_user/lib/navigations/user_navigations.dart
@@ -3,7 +3,8 @@ import 'package:cab_booking_user/screens/flowscreen/users/user_profilepage.dart'
 import 'package:cab_booking_user/screens/flowscreen/users/user_searchpage.dart';
 import 'package:cab_booking_user/utils/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // Import flutter_svg
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class UserNavigation extends StatefulWidget {
   const UserNavigation({super.key});
@@ -31,46 +32,81 @@ class _UserNavigationState extends State<UserNavigation> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/svg/Vector.svg',
-              colorFilter: ColorFilter.mode(grayColor2, BlendMode.srcIn),
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: whiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, -3),
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/svg/Vector.svg',
-              colorFilter: ColorFilter.mode(greencolor, BlendMode.srcIn),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildNavItem(
+              index: 0,
+              inactiveIconPath: 'assets/images/svg/Vector.svg',
+              activeIconPath: 'assets/images/svg/Vector-selected.svg',
+              label: 'Home',
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/svg/majesticons_search-line.svg',
-              colorFilter: ColorFilter.mode(grayColor2, BlendMode.srcIn),
+            _buildNavItem(
+              index: 1,
+              inactiveIconPath: 'assets/images/svg/majesticons_search-line.svg',
+              activeIconPath: 'assets/images/svg/search selected.svg',
+              label: 'Search',
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/svg/majesticons_search-line.svg',
-              colorFilter: ColorFilter.mode(greencolor, BlendMode.srcIn),
+            _buildNavItem(
+              index: 2,
+              inactiveIconPath: 'assets/images/svg/profile-circle.svg',
+              activeIconPath:
+                  'assets/images/svg/profile-circle.svg', // Using the same icon for active state
+              label: 'Profile',
             ),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/images/svg/profile-circle.svg',
-              colorFilter: ColorFilter.mode(grayColor2, BlendMode.srcIn),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required int index,
+    required String inactiveIconPath,
+    required String activeIconPath,
+    required String label,
+  }) {
+    bool isSelected = _selectedIndex == index;
+    return Expanded(
+      child: InkWell(
+        onTap: () => _onItemTapped(index),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              isSelected ? activeIconPath : inactiveIconPath,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                isSelected ? greencolor : grayColor2,
+                BlendMode.srcIn,
+              ),
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/images/svg/profile-circle.svg',
-              colorFilter: ColorFilter.mode(greencolor, BlendMode.srcIn),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: GoogleFonts.outfit(
+                fontSize: 12,
+                color: isSelected ? greencolor : grayColor2,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+              ),
             ),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: greencolor,
-        unselectedItemColor: grayColor2,
-        onTap: _onItemTapped,
+          ],
+        ),
       ),
     );
   }
